9c4d401818b7181983ad75cdab14bab1f5d06f9e
Arran Cudbard-Bell <a.cudbardb@freeradius.org>
2016-09-06 11:00:55 +0100

    Remove old site

--- README.rst
+++
@@ -1,5 +0,0 @@
-The FreeRADIUS Web Site
-=======================
-
-This repository is a copy of the main web site, http://freeradius.org/
-
--- doc/update.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-MAN_FILES=$(find ../../git.freeradius.org/man/ -type f -print)
-
-for x in $MAN_FILES; do
-	HTML="$(basename $x).html"; \
-	man2html < $x > $HTML; \
-done
