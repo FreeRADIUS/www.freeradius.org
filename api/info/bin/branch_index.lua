FreeRADIUS/www.freeradius.org

commit 1ece3bb74a04cea80d9e4fdc5977e95383e63375

Modifications to bring us up to API 0.5

api/info/bin/branch_index.lua
@@ -1,32 +1,38 @@
-local cjson       = require "cjson"
-local ngx         = require "ngx"
-local common      = require "lib.common"
+local cjson             = require "cjson"
+local ngx               = require "ngx"
 
-local index			= common.get_index(common.srv_path .. "/branch/", common.base_url .. "/branch")
-local get_args    = ngx.req.get_uri_args()
+local common            = require "lib.common"
+local keyword_search    = require "lib.keyword_search"
+local indexer           = require "lib.indexer"
+
+local get_args          = ngx.req.get_uri_args()
+local sane_args         = {}
+local ret, err
 
 -- Process common arguments
-local ret, msg, expansion_depth, keyword_expansion_depth, pagenate_start, pagenate_end = common.common_get_args(get_args)
-if ret ~= ngx.OK then
-   common.fatal_error(ret, msg)
+sane_args, err = common.common_get_args(get_args)
+if not sane_args then
+   common.fatal_error(ngx.HTTP_BAD_REQUEST, err)
 end
 
----
----   Server side expansion of URLs
----
-if expansion_depth > 0 then
-   local ret = common.resolve_urls(index, expansion_depth)
-   if ret == ngx.HTTP_NOT_FOUND then
-      common.fatal_error(ret, "Couldn't find resource referenced by expansion URL")
-   elseif ret ~= ngx.OK then
-      common.fatal_error(ret)
-   end
+local search, err = common.search_from_args(get_args.by_keyword,
+                                            get_args.keyword_field,
+                                            { 'name', 'description', 'start', 'end' })
+if err then
+   common.fatal_error(search, err)
 end
 
--- Pagenate last (for stable pagenation)
-if pagenate_start or pagenate_end then
-   index = common.pagenate(index, pagenate_start, pagenate_end)
-end
+local index = indexer.new({}, common.base_url .. "/branch", sane_args.expansion_depth)
 
--- Lua CJSON doesn't have a way of hinting whether empty tables are arrays or hashes
-ngx.say(table.getn(index) > 0 and cjson.encode(index) or "[]");
+index:build(common.srv_path .. "/branch/")
+
+-- Filter by keyword
+ret = search and index:filter(search, sane_args.keyword_expansion_depth)
+
+-- Sort by user specified field or name
+ret = get_args.order_by and index:sort(get_args.order_by) or index:sort('name')
+
+-- Pagenate
+index:pagenate(pagenate_start, pagenate_end)
+
+ngx.say(tostring(index));

api/info/bin/release_index.lua
@@ -1,22 +1,83 @@
-local cjson     = require "cjson"
-local ngx       = require "ngx"
-local common    = require "lib.common"
+local cjson             = require "cjson"
+local ngx               = require "ngx"
 
-local uri       = ngx.var.uri
-local get_args  = ngx.req.get_uri_args()
-local releases	 = {}
+local common            = require "lib.common"
+local keyword_search    = require "lib.keyword_search"
+local indexer           = require "lib.indexer"
+
+local uri               = ngx.var.uri
+local releases	         = {}
 local i, v
-local high_version_v, high_version = 0
-local index
 
-function version_compare(a, b)
+local get_args          = ngx.req.get_uri_args()
+local sane_args
+local ret, err
+
+-- Process common arguments
+sane_args, err = common.common_get_args(get_args)
+if not sane_args then
+   common.fatal_error(ngx.HTTP_BAD_REQUEST, err)
+end
+
+local search, err = common.search_from_args(get_args.by_keyword,
+                                            get_args.keyword_field,
+                                            { 'name', 'description', 'branch', 'category' })
+if err then
+   common.fatal_error(search, err)
+end
+
+--[[Function: version_to_int
+Convert version string into an integer for comparison
+
+@param version to process.
+@return integer representing version or nil
+--]]
+local function version_to_int(version)
+	   local m, err = ngx.re.match(version, "^([0-9]{1,2})\\.([0-9]{1,2})\\.([0-9]{1,2})(?:-(pre|beta|alpha)([0-9]{1,2}))?$", "jo")
+		local int = 0
+		local w = 4
+
+		if err or not m then
+			ngx.log(ngx.ERR, err or "Bad version " .. version)
+			return nil
+		end
+
+		-- Need 5 bytes
+
+		int = (tonumber(m[1]) * (2 ^ 4))
+		int = (tonumber(m[2]) * (2 ^ 3)) + int
+		int = (tonumber(m[3]) * (2 ^ 2)) + int
+
+		if m[4] then
+			if m[4] == 'pre' then
+				w = 3
+			elseif m[4] == 'beta' then
+				w = 2
+			elseif m[4] == 'alpha' then
+				w = 1
+			else
+				ngx.log(ngx.ERR, "Unknown unstable release type \"" .. m[4] '' "\"")
+				return nil
+			end
+		end
+
+		int = (w * 2) + int
+		if m[5] then
+			int = int + tonumber(m[5])
+		end
+
+		return int
+end
+
+-- Compare versions for ordering purposes
+local function version_sort(a, b)
 	local a_int, b_int
 
-	a_int = common.version_to_int(a.name)
+	a_int = version_to_int(a.name)
 	if not a_int then
 		common.fatal_error()
 	end
-	b_int = common.version_to_int(b.name)
+	b_int = version_to_int(b.name)
 	if not b_int then
 		common.fatal_error()
 	end
@@ -24,29 +85,18 @@ function version_compare(a, b)
 	return a_int > b_int
 end
 
--- Process common arguments
-local ret, msg, expansion_depth, keyword_expansion_depth, pagenate_start, pagenate_end = common.common_get_args(get_args)
-if ret ~= ngx.OK then
-   common.fatal_error(ret, msg)
-end
-
 local branch = uri:match("^" .. common.base_url .. "/branch/([^/]+)/")
 
--- Determine the last release in this branch
-index = common.get_index(common.srv_path .. "/branch/" .. branch .. "/release/", common.base_url .. "/branch/" .. branch .. "/release")
-table.sort(index, version_compare)
+local index = indexer.new({}, common.base_url .. "/branch/" .. branch .. "/release", sane_args.expansion_depth)
+index:build(common.srv_path .. "/branch/" .. branch .. "/release/")
 
--- Server side expansion of URL fields using subrequests
-if expansion_depth and expansion_depth > 0 then
-   local ret = common.resolve_urls(index, expansion_depth)
-   if ret ~= ngx.OK then
-      common.fatal_error(ret, "Error retrieving resource referenced by expansion URL")
-   end
-end
+-- Filter by keyword
+ret = search and index:filter(search, keyword_expansion_depth)
 
--- Pagenate last (for stable pagenation)
-if pagenate_start or pagenate_end then
-   index = common.pagenate(index, pagenate_start, pagenate_end)
-end
+-- Sort by user specified field or by version
+ret = get_args.order_by and index:sort(get_args.order_by) or index:sort_with(version_sort)
 
-ngx.say(table.getn(index) > 0 and cjson.encode(index) or "[]");
+-- Pagenate
+index:pagenate(pagenate_start, pagenate_end)
+
+ngx.say(tostring(index));

api/info/bin/branch.lua
@@ -1,20 +1,26 @@
-local cjson    	= require "cjson"
-local ngx      	= require "ngx"
-local common    	= require "lib.common"
+local cjson    	      = require "cjson"
+local ngx      	      = require "ngx"
 
-local uri			= ngx.var.uri
-local get_args		= ngx.req.get_uri_args()
+local common            = require "lib.common"
+
+local uri		         = ngx.var.uri
+
+local get_args          = ngx.req.get_uri_args()
+local sane_args
+local ret, err
 
 -- Process common arguments
-local ret, msg, expansion_depth = common.common_get_args(get_args)
-if ret ~= ngx.OK then
-   common.fatal_error(ret, msg)
+sane_args, err = common.common_get_args(get_args)
+if not sane_args then
+   common.fatal_error(ngx.HTTP_BAD_REQUEST, err)
 end
 
 local branch = uri:match("^" .. common.base_url .. "/branch/([^/]+)/")
-local ret, json  = common.get_json_file(common.srv_path .. "/branch/" .. branch .. ".json")
-if ret ~= ngx.OK then
-   common.fatal_error(ret, "Error retrieving resource")
+local ret, json = common.get_json_file(common.srv_path .. "/branch/" .. branch .. ".json")
+if ret == ngx.HTTP_NOT_FOUND then
+   common.fatal_error(ret, "Branch \"" .. branch .. "\" not found")
+elseif ret ~= ngx.OK then
+   common.fatal_error(ret)
 end
 
 -- Add link to releases to allow expansion
@@ -23,17 +29,16 @@ ret, json.last_release = common.get_json_subrequest(json.release.url .. "?pagena
 if ret == ngx.HTTP_NOT_FOUND then
    json.last_release = nil
 elseif ret ~= ngx.OK then
-   common.fatal(ret, "Error retrieving resource")
+   common.fatal_error(ret, "Error retrieving release list")
 else
-   json.last_release = json.last_release[1]
+   json.last_release = json.last_release[1]  -- Releases are ordered DESC
 end
 
-
 -- Server side expansion of URL fields using subrequests
-if expansion_depth and expansion_depth > 0 then
-   local ret = common.resolve_urls(json, expansion_depth)
+if sane_args.expansion_depth and sane_args.expansion_depth > 0 then
+   local ret = common.resolve_urls(json, sane_args.expansion_depth)
    if ret ~= ngx.OK then
-      common.fatal_error(ret, "Error retrieving resource referenced by expansion URL")
+      common.fatal_error(ret, "Error retrieving nested object (" .. tostring(ret) .. ")")
    end
 end
 

api/info/bin/component.lua
@@ -1,27 +1,32 @@
-local cjson    	= require "cjson"
-local ngx      	= require "ngx"
-local common   	= require "lib.common"
+local cjson    	      = require "cjson"
+local ngx      	      = require "ngx"
 
-local uri 			= ngx.var.uri
-local get_args		= ngx.req.get_uri_args()
+local common   	      = require "lib.common"
+local uri 			      = ngx.var.uri
+
+local get_args          = ngx.req.get_uri_args()
+local sane_args
+local ret, err
 
 -- Process common arguments
-local ret, msg, expansion_depth = common.common_get_args(get_args)
-if ret ~= ngx.OK then
-   common.fatal_error(ret, msg)
+sane_args, err = common.common_get_args(get_args)
+if not sane_args then
+   common.fatal_error(ngx.HTTP_BAD_REQUEST, err)
 end
 
 local component = uri:match("^" .. common.base_url .. "/component/([^/]+)/")
 local ret, json  = common.get_json_file(common.srv_path .. "/" .. "component" .. "/" .. component .. ".json")
-if ret ~= ngx.OK then
-   common.fatal_error(ret, "Error retrieving resource")
+if ret == ngx.HTTP_NOT_FOUND then
+   common.fatal_error(ret, "Component \"" .. component .. "\" not found")
+elseif ret ~= ngx.OK then
+   common.fatal_error(ret)
 end
 
 -- Server side expansion of URL fields using subrequests
-if expansion_depth and expansion_depth > 0 then
-   local ret = common.resolve_urls(json, expansion_depth)
+if sane_args.expansion_depth and sane_args.expansion_depth > 0 then
+   local ret = common.resolve_urls(json, sane_args.expansion_depth)
    if ret ~= ngx.OK then
-      common.fatal_error(ret, "Error retrieving resource referenced by expansion URL")
+      common.fatal_error(ret, "Error retrieving nested object (" .. tostring(ret) .. ")")
    end
 end
 

api/info/bin/component_index.lua
@@ -1,93 +1,37 @@
-local cjson       = require "cjson"
-local ngx         = require "ngx"
-local common      = require "lib.common"
+local cjson             = require "cjson"
+local ngx               = require "ngx"
 
-local index
-local get_args    = ngx.req.get_uri_args()
+local common            = require "lib.common"
+local keyword_search    = require "lib.keyword_search"
+local indexer           = require "lib.indexer"
 
-local by_keyword_func, by_keyword_pattern
+local get_args          = ngx.req.get_uri_args()
+local sane_args
+local ret, err
 
 -- Process common arguments
-local ret, msg, expansion_depth, keyword_expansion_depth, pagenate_start, pagenate_end = common.common_get_args(get_args)
-if ret ~= ngx.OK then
-   common.fatal_error(ret, msg)
-end
-
--- Process by_keyword pattern (if it was provided)
-if get_args.by_keyword then
-   by_keyword_func, by_keyword_pattern = common.keyword_validate(get_args.by_keyword)
-
-   -- Check the pattern's sane
-   if by_keyword_func == nil then
-      common.fatal_error(ngx.HTTP_BAD_REQUEST, by_keyword_pattern)
-   end
-end
-
---
---    Sanity checks
---
-if get_args.by_category and get_args.by_dependency_on then
+if get_args.by_dependency_on and get_args.by_category then
    common.fatal_error(ngx.HTTP_BAD_REQUEST, "by_category and by_dependency_on filters are mutually exclusive")
 end
 
---
---    Filter by category
---
-if get_args.by_category then
-   local i, v
-   local cache = ngx.shared.info_api_component_category
+sane_args, err = common.common_get_args(get_args)
+if not sane_args then
+   common.fatal_error(ngx.HTTP_BAD_REQUEST, err)
+end
 
-   if not cache then
-      ngx.log(ngx.ERR, "Cache not declared.  Declare with 'lua_shared_dict info_api_component_category;' in http {}")
-   end
+local search, err = common.search_from_args(get_args.by_keyword,
+                                            get_args.keyword_field,
+                                            { 'name', 'description', 'branch', 'category' })
+if err then
+   common.fatal_error(search, err)
+end
 
-   if not ngx.re.find(get_args.by_category, "^[a-z-]+$", "jio") then
-      common.fatal_error(ngx.HTTP_BAD_REQUEST, "Category names are restricted to [a-z-]")
-   end
+local index = indexer.new({}, common.base_url .. "/component", sane_args.expansion_depth)
 
-   index = {}
-   for i, v in ipairs(common.get_index(common.srv_path .. "/component/", common.base_url .. "/component")) do
-      local category
-
-      if not v["url"] then
-         ngx.log(ngx.ERR, "Component " .. v["name"] .. " missing url key")
-         common.fatal_error()
-      end
-
-      if cache then
-         category = cache:get(v["url"])
-      end
-
-      if not category then
-         local ret, json = common.get_json_subrequest(v["url"])
-         if ret ~= ngx.OK then
-            common.fatal_error(ngx.HTTP_INTERNAL_SERVER_ERROR,
-                               "Subrequest for \"" .. v["url"] .. "\" failed with" .. tostring(ret))
-         end
-
-         -- Get the category key
-         if not json["category"] then
-            ngx.log(ngx.ERR, "Component " .. v["name"] .. " missing category key")
-            common.fatal_error()
-         end
-
-         category = json["category"]
-
-         -- Cache the category (as that was all pretty expensive)
-         if cache then
-            cache:set(v["url"], category)
-         end
-      end
-
-      -- Only add matching to the filtered result set
-      if category == get_args.by_category then
-          table.insert(index, v)
-      end
-   end
 --
 --    Filter by dependency
 --
-elseif get_args.by_dependency_on then
+if get_args.by_dependency_on then
    local url, ret, json
 
    if not ngx.re.find(get_args.by_dependency_on, "^[0-9a-z_.-]+$", "jio") then
@@ -105,66 +49,42 @@ elseif get_args.by_dependency_on then
       common.fatal_error(ngx.HTTP_BAD_REQUEST, "Component has no dependents")
    end
 
-   index = json.dependents
+   index:set(json.dependents)
 --
---    No filtering
+--    Filter by category
 --
+elseif get_args.by_category then
+   local category
+
+   index:build(common.srv_path .. "/component/")
+
+   category = keyword_search.new()
+   category:set_fields_default('category')
+
+   if not ngx.re.find(get_args.by_category, "^[a-z-]+$", "jio") then
+      common.fatal_error(ngx.HTTP_BAD_REQUEST, "Category names are restricted to [a-z-]")
+   end
+
+   ret, err = category:set_pattern(get_args.by_category)
+   if ret == false then
+      common.fatal_error(ngx.HTTP_BAD_REQUEST, err)
+   end
+
+   index:filter(category, 1)
+---
+---   Don't filter
+---
 else
-   index = common.get_index(common.srv_path .. "/component/", common.base_url .. "/component")
+   index:build(common.srv_path .. "/component/")
 end
 
---
---    Filter by keyword (possibly combined with previous filtering)
---
-if get_args.by_keyword then
-   local filtered = {}
+-- Filter by keyword
+ret = search and index:filter(search, sane_args.keyword_expansion_depth)
 
-   for i, v in ipairs(index) do
-      local to_expand
+-- Sort by user specified field
+ret = get_args.order_by and index:sort(get_args.order_by)
 
-      if keyword_expansion_depth > expansion_depth then
-         to_expand = common.table_copy(v)
-      else
-         to_expand = v
-      end
+-- Pagenate
+index:pagenate(sane_args.pagenate_start, sane_args.pagenate_end)
 
-      if keyword_expansion_depth > 0 then
-         local ret = common.resolve_urls(to_expand, keyword_expansion_depth)
-         if ret == ngx.HTTP_NOT_FOUND then
-            common.fatal_error(ret, "Couldn't find resource referenced by expansion URL")
-         elseif ret ~= ngx.OK then
-            common.fatal_error(ret)
-         end
-      end
-
-      local ret, msg = common.keyword_search(to_expand, by_keyword_func,
-                                             by_keyword_pattern, { 'name', 'description', 'branch', 'category' })
-      if ret == nil then
-         common.fatal_error(ngx.HTTP_BAD_REQUEST, msg .. "COCK")
-      elseif ret then
-         table.insert(filtered, v)
-      end
-   end
-
-   index = filtered
-end
-
----
----   Server side expansion of URLs
----
-if expansion_depth > 0 and expansion_depth ~= keyword_expansion_depth then
-   local ret = common.resolve_urls(index, expansion_depth)
-   if ret == ngx.HTTP_NOT_FOUND then
-      common.fatal_error(ret, "Couldn't find resource referenced by expansion URL")
-   elseif ret ~= ngx.OK then
-      common.fatal_error(ret)
-   end
-end
-
--- Pagenate last (for stable pagenation)
-if pagenate_start or pagenate_end then
-   index = common.pagenate(index, pagenate_start, pagenate_end)
-end
-
--- Lua CJSON doesn't have a way of hinting whether empty tables are arrays or hashes
-ngx.say(table.getn(index) > 0 and cjson.encode(index) or "[]");
+ngx.say(tostring(index));

api/info/bin/release.lua
@@ -1,27 +1,33 @@
-local cjson    	= require "cjson"
-local ngx      	= require "ngx"
-local common   	= require "lib.common"
+local cjson    	      = require "cjson"
+local ngx            	= require "ngx"
 
-local uri 			= ngx.var.uri
-local get_args		= ngx.req.get_uri_args()
+local common         	= require "lib.common"
+
+local uri 			      = ngx.var.uri
+
+local get_args          = ngx.req.get_uri_args()
+local sane_args
+local ret, err
 
 -- Process common arguments
-local ret, msg, expansion_depth = common.common_get_args(get_args)
-if ret ~= ngx.OK then
-   common.fatal_error(ret, msg)
+sane_args, err = common.common_get_args(get_args)
+if not sane_args then
+   common.fatal_error(ngx.HTTP_BAD_REQUEST, err)
 end
 
 local branch, release = uri:match("^" .. common.base_url .. "/branch/([^/]+)/release/([^/]+)/")
 local ret, json  = common.get_json_file(common.srv_path .. "/branch/" .. branch.. "/release/" .. release .. ".json")
-if ret ~= ngx.OK then
-   common.fatal_error(ret, "Error retrieving resource")
+if ret == ngx.HTTP_NOT_FOUND then
+   common.fatal_error(ret, "Release \"" .. rlease .. "\" not found")
+elseif ret ~= ngx.OK then
+   common.fatal_error(ret)
 end
 
 -- Server side expansion of URL fields using subrequests
-if expansion_depth and expansion_depth > 0 then
-   local ret = common.resolve_urls(json, expansion_depth)
+if sane_args.expansion_depth and sane_args.expansion_depth > 0 then
+   local ret = common.resolve_urls(json, sane_args.expansion_depth)
    if ret ~= ngx.OK then
-      common.fatal_error(ret, "Error retrieving resource referenced by expansion URL")
+      common.fatal_error(ret, "Error retrieving nested object (" .. tostring(ret) .. ")")
    end
 end
 

commit d432a8fdcb52e156ae05bc7fba224759d71068ff

fix pagination (broken in 1ece3bb7)

api/info/bin/branch_index.lua
@@ -51,6 +51,6 @@ ret = search and index:filter(search, sane_args.keyword_expansion_depth)
 ret = sane_args.order_by and index:sort(sane_args.order_by) or index:sort('name')
 
 -- Pagenate
-index:paginate(paginate_start, paginate_end)
+index:paginate(sane_args.paginate_start, sane_args.paginate_end)
 
 ngx.say(tostring(index));

api/info/bin/release_index.lua
@@ -146,6 +146,6 @@ ret = search and index:filter(search, sane_args.keyword_expansion_depth)
 ret = sane_args.order_by and index:sort(sane_args.order_by) or index:sort_with(version_sort)
 
 -- Pagenate
-index:paginate(paginate_start, paginate_end)
+index:paginate(sane_args.paginate_start, sane_args.paginate_end)
 
 ngx.say(tostring(index));