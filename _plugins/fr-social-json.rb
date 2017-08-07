88e7bd4074e24d43b1f8b380a9394b68b7d68094
Matthew Newton <matthew-git@newtoncomputing.co.uk>
2017-08-07 14:04:04 +0100

    move secrets out of the repo

--- _plugins/fr-social-json.rb
+++ _plugins/fr-social-json.rb
@@ -1,18 +1,23 @@
 module Jekyll
+    require 'twitter'
+    require 'httparty'
     require 'json'
     class JSONGenerator < Generator
         def generate(site)
 
+            # Twitter
+
+            twitter_data = JSON.parse(File.read(site.config['source'] + "/_wwwdata/twitter.json"))
             socialList = {}
 
             client = Twitter::REST::Client.new do |config|
-                config.consumer_key        = "V9r0TUwgivuQHsmS702hVBB6h"
-                config.consumer_secret     = "TnBNR4b4Tb5w5COA8BWpY1RrVLWHHwdrmtGnSL2ANdTT5wBDhD"
-                config.access_token        = "83314299-kW2VHgIxlMGdID1Sjlaa3eCpywD1bULcYAQqPSb7N"
-                config.access_token_secret = "914ZhqAkpw4gqhSVUH4lEM5nrC4vwxxhYOUoo9Rc5Ho4d"
+                config.consumer_key = twitter_data['consumer_key']
+                config.consumer_secret = twitter_data['consumer_secret']
+                config.access_token = twitter_data['access_token']
+                config.access_token_secret = twitter_data['access_token_secret']
             end
 
-            timeline = client.user_timeline("freeradius", options= {include_rts: true, exclude_replies: true})
+            timeline = client.user_timeline(twitter_data['timeline_user'], options= {include_rts: true, exclude_replies: true})
 
             tweets = []
             timeline.take(10).collect do |tweet|
@@ -20,13 +25,20 @@ module Jekyll
                 # so ensure it doesn't change with system settings.
                 tweets.push({"created_at": "#{tweet.created_at.strftime('%a %b %d %H:%M:%S %z %Y')}", "text": "#{tweet.text}"})
             end
-            # return tweet
             socialList['twitter'] = JSON.parse(JSON.generate(tweets))
 
-            response_so = HTTParty.get('https://api.stackexchange.com//2.2/search/excerpts?pagesize=10&order=desc&sort=activity&tagged=freeradius&site=stackoverflow')
+
+            # StackExchange / StackOverflow
+
+            sx_data = JSON.parse(File.read(site.config['source'] + "/_wwwdata/stackexchange.json"))
+            response_so = HTTParty.get(sx_data['url'])
             socialList['stackoverflow'] = JSON.parse(JSON.generate(response_so['items'].first(10)))
 
-            response_gh = HTTParty.get('https://api.github.com/repos/FreeRADIUS/freeradius-server/commits?client_id=37bcc833b50aaa2f8111&client_secret=7dad0b817aa3b8f8bf1aeb18bbf6bc67c1b5a415')
+
+            # GitHub
+
+            github_data = JSON.parse(File.read(site.config['source'] + "/_wwwdata/github.json"))
+            response_gh = HTTParty.get(github_data['url'])
             socialList['github'] = JSON.parse(JSON.generate(response_gh.first(10)))
 
             File.open("social.json","w") do |f|
